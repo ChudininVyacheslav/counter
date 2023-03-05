efd08e6b0a16c2c5c9399b719af9e3edb059a14b
Chudinin Vyacheslav <chudinin@inbox.ru>
2023-03-05 22:53:50 +0300

    d d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from './toolkitRedux/toolkitReducer';
 
 function App() {
+  const count = useSelector(state => state.toolkit.count)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Счетчик равен {count}</h1>
+      <button onClick={() => dispatch(increment())}>Увеличить</button>
+      <button onClick={() => dispatch(decrement())}>Уменьшить</button>
     </div>
   );
 }
