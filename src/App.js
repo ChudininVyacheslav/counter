a8f405102b82414a051a05937d43f8d97e36e4ff
Chudinin Vyacheslav <125117916+ChudininVyacheslav@users.noreply.github.com>
2023-03-06 12:23:17 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from './toolkitReducer';
+
+function App() {
+  const count = useSelector(state => state.toolkit.count)
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Счетчик равен {count}</h1>
+      <button onClick={() => dispatch(increment())}>Увеличить</button>
+      <button onClick={() => dispatch(decrement())}>Уменьшить</button>
+    </div>
+  );
+};
+
+export default App;
